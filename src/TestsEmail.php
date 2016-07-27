49ac4561b2009dc4dee77c28d7380a5a98944708
Tijmen Wierenga <t.wierenga@live.nl>
2016-07-27 19:30:48 +0200

    Created Trait for testing emails

---
+++ src/TestsEmail.php
@@ -0,0 +1,31 @@
+<?php
+namespace TijmenWierenga\LaravelMailTester;
+
+
+use Swift_Message;
+
+trait TestsEmail
+{
+    /**
+     * @var array
+     */
+    protected $emails = [];
+
+    /**
+     * @before
+     */
+    public function setMailPlugin()
+    {
+        app()->mailer
+            ->getSwiftMailer()
+            ->registerPlugin(new TestCaseMailEventListener($this));
+    }
+
+    /**
+     * @param Swift_Message $email
+     */
+    public function addEmail(Swift_Message $email)
+    {
+        $this->emails[] = $email;
+    }
+}
